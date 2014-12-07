9c7156056f7b82eb35685c1e956619aef2cc5663
Stefan Seifert <nine@cpan.org>
2014-12-07 11:42:31 +0000

    Import of NINE/Inline-Python-0.46.tar.gz from CPAN.
    gitpan-cpan-distribution: Inline-Python
    gitpan-cpan-version:      0.46
    gitpan-cpan-path:         NINE/Inline-Python-0.46.tar.gz
    gitpan-cpan-author:       NINE
    gitpan-cpan-maturity:     released

--- perlmodule.h
+++ perlmodule.h
@@ -33,29 +33,48 @@ typedef struct {
     PyObject* (*cfun)(PyObject *self, PyObject *args); /* a regular Python function */
 } PerlSub_object;
 
-extern DL_IMPORT(PyTypeObject) PerlPkg_type, PerlObj_type, PerlSub_type;
+extern PyTypeObject PerlPkg_type, PerlObj_type, PerlSub_type;
 
-#define PerlPkgObject_Check(v) ((v)->ob_type == &PerlPkg_type)
-#define PerlObjObject_Check(v) ((v)->ob_type == &PerlObj_type)
-#define PerlSubObject_Check(v) ((v)->ob_type == &PerlSub_type)
+#ifndef PyVarObject_HEAD_INIT  /* Python 2.5 does not define this*/
+    #define PyVarObject_HEAD_INIT(type, size) \
+        PyObject_HEAD_INIT(type) size,
+#endif
 
+#ifndef Py_TYPE /* Python 2.5 does not define this*/
+#define Py_TYPE(ob) 			(((PyObject*)(ob))->ob_type)
+#endif
+
+#define PerlPkgObject_Check(v) (Py_TYPE(v) == &PerlPkg_type)
+#define PerlObjObject_Check(v) (Py_TYPE(v) == &PerlObj_type)
+#define PerlSubObject_Check(v) (Py_TYPE(v) == &PerlSub_type)
+
+#if PY_MAJOR_VERSION >= 3
+#define PKG_EQ(obj,pkg) (strcmp(PyBytes_AsString((obj)->full), (pkg))==0)
+#else
 #define PKG_EQ(obj,pkg) (strcmp(PyString_AsString((obj)->full), (pkg))==0)
+#endif
+
+/* Macro for returning Py_NotImplemented from a function */
+#ifndef Py_RETURN_NOTIMPLEMENTED /* Python 3.1 does not define this*/
+#define Py_RETURN_NOTIMPLEMENTED \
+    return Py_INCREF(Py_NotImplemented), Py_NotImplemented
+#endif
+
 
 /***************************************
  *         METHOD DECLARATIONS         *
  ***************************************/
 
 /* methods of _perl_pkg */
-extern DL_IMPORT(PyObject *) newPerlPkg_object(PyObject *, PyObject *);
+extern PyObject * newPerlPkg_object(PyObject *, PyObject *);
 
 /* methods of _perl_obj */
-extern DL_IMPORT(PyObject *) newPerlObj_object(SV *, PyObject *);
+extern PyObject * newPerlObj_object(SV *, PyObject *);
 
 /* methods of _perl_sub */
-extern DL_IMPORT(PyObject *) newPerlSub_object(PyObject *, PyObject *, SV *);
-extern DL_IMPORT(PyObject *) newPerlMethod_object(PyObject*,PyObject*,SV*);
-extern DL_IMPORT(PyObject *) newPerlCfun_object(PyObject* (*)(PyObject *,
-							      PyObject *));
+extern PyObject * newPerlSub_object(PyObject *, PyObject *, SV *);
+extern PyObject * newPerlMethod_object(PyObject*, PyObject*, SV*);
+extern PyObject * newPerlCfun_object(PyObject* (*)(PyObject *, PyObject *));
 
 #ifdef __cplusplus
 }
--- t/23getattr.t
+++ t/23getattr.t
@@ -32,20 +32,20 @@ use Inline::Python qw(py_eval py_call_function);
 
 py_eval(<<'END');
 
-def test_attrs(foo):
+def test_attrs(foo, s):
     perl.ok(hasattr(foo, 'test'))
     perl.ok(not hasattr(foo, 'foo'))
-    perl.ok(foo.test == 'Attrs test!')
-    perl.ok(foo.__getattr__('test') == 'Attrs test!')
+    perl.ok(foo.test == s)
+    perl.ok(foo.__getattr__('test') == s)
 
 def test_noattrs(bar):
     try:
         perl.warn(bar.test)
-    except KeyError:
+    except AttributeError:
         return 1
     return 0
 
 END
 
-ok(py_call_function("__main__", "test_attrs", Attrs->new), undef);
+ok(py_call_function("__main__", "test_attrs", Attrs->new, 'Attrs test!'), undef);
 ok(py_call_function("__main__", "test_noattrs", NoAttrs->new) == 1);
